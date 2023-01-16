e32b3e75563842b4eb7613ee24a20c4d595354ce
Daratchanan <daratchanan.p@gmail.com>
2023-01-16 17:51:12 +0700

    product detail

--- components/screens/ProductInfo.js
+++ components/screens/ProductInfo.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import {
 	View,
-	Text,
 	StatusBar,
 	ScrollView,
 	TouchableOpacity,
 	FlatList,
 	Image,
-	Dimensions
+	Dimensions,
+	Animated,
+	Text
 } from 'react-native';
 import { COLOURS, Items } from '../database/Database';
 import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const ProductInfo = ({ route, navigation }) => {
 	const { productID, data } = route.params;
@@ -18,6 +20,10 @@ const ProductInfo = ({ route, navigation }) => {
 	const [product, setProduct] = useState({});
 
 	const { width } = Dimensions.get('window');
+
+	const scrollX = new Animated.Value(0);
+
+	let position = Animated.divide(scrollX, width);
 
 	//get product data by productID
 	// const getDataFromDB = async () => {
@@ -31,7 +37,6 @@ const ProductInfo = ({ route, navigation }) => {
 
 	const getDataFromDB = () => {
 		const productDetail = Items.find(f => f.id === productID);
-		// console.log('productDetail =>',productDetail);
 		setProduct(productDetail);
 	};
 
@@ -45,10 +50,7 @@ const ProductInfo = ({ route, navigation }) => {
 
 
 	//product horizental scroll product card
-	const renderProduct = (d) => {
-		// console.log('item=>', item);
-		const {item,index} = d;
-		
+	const renderProduct = ({ item, index }) => {
 		return (
 			<View
 				style={{
@@ -58,8 +60,6 @@ const ProductInfo = ({ route, navigation }) => {
 					alignItems: 'center'
 				}}
 			>
-				{/* <Text>{`index: ${d.index} : ${d.item.title}`}</Text> */}
-				{/* <Text>{item}</Text> */}
 				<Image
 					source={item}
 					style={{
@@ -75,8 +75,8 @@ const ProductInfo = ({ route, navigation }) => {
 	// console.log('width=>', width);
 	// console.log('productID=>', productID);
 	// console.log('data=> ', data);
-	console.log('product=> ', product);
-
+	// console.log('product=> ', product);
+	// console.log('scrollX=>', scrollX);
 	return (
 		<View
 			style={{
@@ -125,31 +125,181 @@ const ProductInfo = ({ route, navigation }) => {
 						</TouchableOpacity>
 					</View>
 					<FlatList
-						data={product.productImageList}
-						// data={['aa', 'bb', 'cc']}
-						// data={[
-						// 	{
-						// 		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-						// 		title: 'First Item',
-						// 	 },
-						// 	 {
-						// 		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-						// 		title: 'Second Item',
-						// 	 },
-						// 	 {
-						// 		id: '58694a0f-3da1-471f-bd96-145571e29d72',
-						// 		title: 'Third Item',
-						// 	 },
-						// ]}
+						data={product.productImageList ? product.productImageList : null}
 						horizontal
 						renderItem={renderProduct}
-
+						showsHorizontalScrollIndicator={false}
+						decelerationRate={1}
+						snapToInterval={width}
+						bounces={false}
+						onScroll={Animated.event(
+							[{ nativeEvent: { contentOffset: { x: scrollX } } }],
+							{ useNativeDriver: false },
+						)}
 					/>
+					<View
+						style={{
+							width: '100%',
+							flexDirection: 'row',
+							justifyContent: 'center',
+							alignItems: 'center',
+							marginBottom: 16,
+							marginTop: 32
+						}}
+					>
+						{product.productImageList ?
+							product.productImageList.map((data, index) => {
+								let opacity = position.interpolate({
+									inputRange: [index - 1, index, index + 1],
+									outputRange: [0.2, 1, 0.2],
+									extrapolate: 'clamp',
+								});
+								return (
+									<Animated.View
+										key={index}
+										style={{
+											width: '16%',
+											height: 2.4,
+											backgroundColor: COLOURS.black,
+											opacity,
+											marginHorizontal: 4,
+											borderRadius: 100,
+										}}
+									/>
+								);
+							})
+							: null}
+					</View>
+				</View>
+
+				<View
+					style={{
+						paddingHorizontal: 16,
+						marginTop: 6,
+					}}
+				>
+					<View
+						style={{
+							flexDirection: 'row',
+							alignItems: 'center',
+							marginVertical: 14
+						}}
+					>
+						<Ionicons
+							name='cart'
+							style={{
+								fontSize: 18,
+								color: COLOURS.blue,
+								marginRight: 6,
+							}}
+						/>
+						<Text
+							style={{
+								fontSize: 12,
+								color: COLOURS.black
+							}}
+						>
+							Shopping
+						</Text>
+
+					</View>
+
+					<View
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+							marginVertical: 4
+						}}
+					>
+						<Text
+							style={{
+								fontSize: 24,
+								fontWeight: '600',
+								letterSpacing: 0.5,
+								color: COLOURS.black,
+								marginVertical: 4,
+								maxWidth: '84%'
+							}}
+						>
+							{product.productName}
+						</Text>
+						<Ionicons
+							name='link-outline'
+							style={{
+								fontSize: 24,
+								color: COLOURS.blue,
+								backgroundColor: COLOURS.blue + 10,
+								padding: 8,
+								borderRadius: 50
+							}}
+						/>
+					</View>
+
+					<Text
+						style={{
+							fontSize: 12,
+							fontWeight: '400',
+							color: COLOURS.black,
+							letterSpacing: 1,
+							opacity: 0.5,
+							lineHeight: 20,
+							maxWidth: '85%',
+							maxHeight: 44,
+							marginBottom: 18,
+						}}
+					>
+						{product.description}
+					</Text>
+					<View
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+							marginVertical: 14,
+							borderBottomColor: COLOURS.backgroundLight,
+							borderBottomWidth: 1,
+							paddingBottom: 20,
+						}}
+					>
+						<View
+							style={{
+								flexDirection: 'row',
+								width: '80%',
+								alignItems: 'center'
+							}}
+						>
+							<View
+								style={{
+									justifyContent: 'center',
+									alignItems: 'center',
+									color: COLOURS.blue,
+									backgroundColor: COLOURS.backgroundLight,
+									padding: 12,
+									borderRadius: 50,
+									marginRight: 10,
+								}}
+							>
+								<Entypo
+									name='location-pin'
+									style={{
+										fontSize: 16,
+										color: COLOURS.blue
+									}}
+								/>
+							</View>
+							<Text>Rustaveli Ave 57,{'\n'}17-001, Batume</Text>
+						</View>
+						<Entypo
+							name='chevron-right'
+							style={{
+								fontSize: 22,
+								color: COLOURS.backgroundDark,
+							}}
+						/>
+					</View>
 				</View>
 			</ScrollView>
-			{/* <Text>
-				// {JSON.stringify(product)}
-			</Text> */}
 		</View>
 	);
 };
